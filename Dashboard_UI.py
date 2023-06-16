4765250b69c2672adc8e7ec8bd236993e0707d36
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-06-16 10:37:08 +0500

    added

--- Dashboard_UI.py
+++ Dashboard_UI.py
@@ -80,7 +80,7 @@ def bar_chart(reddit_df):
 
 
 def main():
-    st.title("CryptoSentimentTracker")
+    st.title(":bar_chart:CryptoSentimentTracker")
     st.markdown("##")
     # Authenticate with Reddit API
     reddit_client = authenticate_reddit()
